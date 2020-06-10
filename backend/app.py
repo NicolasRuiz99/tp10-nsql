cf4dad7b28b88af1651a91241320e62fa20b8f3a
Nicolás Ruiz <nicoruiz72@gmail.com>
2020-06-10 18:56:47 -0300

    continuo tp

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,11 @@
 from flask import Flask
 from flask import render_template, jsonify, request, redirect, url_for
 import json
-from connectiondb import crear_usuarios
+from connectiondb import crear_usuarios,get_user,cargar_coins,get_assets,cargar_armas
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-
-def get_user (name):
-    user = None
-    with open ('users.json') as f:
-        file_data = json.load(f)
-        for element in file_data:
-            if element["name"] == name:
-                user = element
-    if user == None:
-        raise Exception ('not found')
-    else:
-        return user
 
 @app.route('/get_user', methods=['POST'])
 def get():
@@ -28,6 +16,39 @@ def get():
     except (Exception) as err:
         return str(err), 500
 
+@app.route('/cargar_coins', methods=['GET'])
+def punto3():
+    try:
+        res = cargar_coins()
+        return jsonify (res)
+    except (Exception) as err:
+        return str(err), 500
+
+@app.route('/cargar_armas', methods=['GET'])
+def punto4():
+    try:
+        res = cargar_armas()
+        return jsonify (res)
+    except (Exception) as err:
+        return str(err), 500
+
+@app.route('/get_assets', methods=['GET'])
+def assets():
+    try:
+        res = get_assets ()
+        return jsonify (res)
+    except (Exception) as err:
+        return str(err), 500
+
+@app.route('/get_user_coins', methods=['POST'])
+def user_coins():
+    try:
+        name = request.json["name"]
+        user = get_user(name)
+        res = get_assets ()
+        return jsonify (res)
+    except (Exception) as err:
+        return str(err), 500
 
 @app.route('/about')
 def about():
--- backend/connectiondb.py
+++ backend/connectiondb.py
@@ -1,5 +1,6 @@
 from bigchaindb_driver import BigchainDB
-from bigchaindb_driver.crypto import generate_keypair
+from bigchaindb_driver.crypto import generate_keypair,CryptoKeypair
+import json
 
 bdb = BigchainDB('https://test.ipdb.io')
 
@@ -11,13 +12,109 @@ def crear_usuarios():
     users.append (generate_keypair())
     return users
 
-def get_list(nombre):
-    db = connect_db()
-    db.lpush(nombre, "luke, leia, han, chewbbaca")
-    lista = db.lrange(nombre,0,-1)
-    return lista
+def get_user (name):
+    user = None
+    with open ('users.json') as f:
+        file_data = json.load(f)
+        for element in file_data:
+            if element["name"] == name:
+                user = element
+    if user == None:
+        raise Exception ('not found')
+    else:
+        user = CryptoKeypair (private_key=user["private_key"],public_key=user["public_key"])
+        return user
+
+def cargar_coins ():
+
+    darth_vader = get_user ("Darth Vader")
+    boba_fett = get_user ("Boba Fett")
+    greedo = get_user ("Greedo")
+    din_djarin = get_user ("Din Djarin")
+
+    coin_asset = {
+    'data': {
+        'name': "Galactic Coin",
+    },
+    }
+
+    prepared_creation_tx = bdb.transactions.prepare(
+        operation='CREATE',
+        signers=darth_vader.public_key,
+        asset=coin_asset,
+        recipients=[([boba_fett.public_key],5),([greedo.public_key],3),([din_djarin.public_key],8)]
+    )
+
+    fulfilled_creation_tx = bdb.transactions.fulfill(
+        prepared_creation_tx,
+        private_keys=darth_vader.private_key
+    )
+
+    sent_creation_tx = bdb.transactions.send_commit(fulfilled_creation_tx)
+
+    return sent_creation_tx
+
+def cargar_armas ():
+
+    darth_vader = get_user ("Darth Vader")
+
+    metadata = {'type':"weapon"}
+
+    weapon_assets = [
+        {
+    'data': {
+        'name': "Sable de Luz",
+        'cost': 6
+    },
+    },
+    {
+    'data': {
+        'name': "Blaster",
+        'cost': 3
+    },
+    },
+    {
+    'data': {
+        'name': "Carabina Blaster EE-3",
+        'cost': 4
+    },
+    },
+    {
+    'data': {
+        'name': "Cañon Blaster",
+        'cost': 8
+    },
+    },
+    {
+    'data': {
+        'name': "Rifle Blaster E-22",
+        'cost': 5
+    },
+    }
+    ]
+    
+    for a in weapon_assets:
+        prepared_creation_tx = bdb.transactions.prepare(
+            operation='CREATE',
+            signers=darth_vader.public_key,
+            recipients=[([darth_vader.public_key], 10)],
+            asset=a,
+            metadata=metadata
+        )
+
+        fulfilled_creation_tx = bdb.transactions.fulfill(
+            prepared_creation_tx,
+            private_keys=darth_vader.private_key
+        )
+        sent_creation_tx = bdb.transactions.send_commit(fulfilled_creation_tx)
+
+    return sent_creation_tx
+
+def get_assets (user):
+    res = []
+    assets = bdb.assets.get(search='Galactic Coin')
+    for a in assets:
+        res.append (bdb.transactions.get(asset_id=a['id']))
+    return res
 
 
-def get_kylo():
-    db = connect_db()
-    return db.get("kylo").decode('utf-8')
